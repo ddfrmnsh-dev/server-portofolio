d0a31948a3ff5a01bf3bd49b57c64df315b86977
Dede Firmansah <dedefirmansah0508@gmail.com>
2024-05-18 12:07:03 +0700

    fix: midlleware with jwt

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,82 +1,39 @@
 import { Request, Response, NextFunction } from "express";
 import jwt, { Secret } from "jsonwebtoken";
-const authMiddlewares = (req: Request, res: Response, next: NextFunction) => {
-  // const token = req.header('Authorization')
-  const token = req.headers.authorization?.split(" ")[1];
-
-  if (!token) {
-    return res
-      .status(401)
-      .json({ message: "Token tidak tesedia, access denied" });
-  }
-
-  try {
-    let secret = process.env.TOKEN_SECRET;
-    jwt.verify(token, <Secret>secret, (err, decoded) => {
-      if (err) {
-        return res.status(401).json({ message: "Token tidak valid" });
-      }
-      next();
-    });
-  } catch (e) {
-    return res.status(401).json({ message: "Token invalid" });
-  }
-};
 
 const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
-  const authorizationHeader = req.headers.authorization;
-  console.log("Header Authorization:", authorizationHeader);
+  const token = req.cookies.token;
 
-  if (!authorizationHeader) {
-    return res
-      .status(401)
-      .json({ message: "Header Authorization tidak tersedia, akses ditolak" });
-  }
-
-  const [bearer, token] = authorizationHeader.split(" ");
-
-  if (bearer !== "Bearer" || !token) {
-    return res
-      .status(401)
-      .json({ message: "Format token tidak valid, akses ditolak" });
+  if (!token) {
+    req.flash("alertMessage", "Token tidak tersedia, akses ditolak");
+    req.flash("alertStatus", "danger");
+    return res.redirect("/admin/signin");
   }
 
   try {
-    // const decoded = jwt.verify(token, process.env.TOKEN_SECRET as string);
-    // req.decoded = decoded;
-    // next();
-    let secret = process.env.TOKEN_SECRET;
-    jwt.verify(token, <Secret>secret, (err, decoded) => {
-      req.decoded = decoded;
+    const secret: Secret = process.env.TOKEN_SECRET || "defaultSecret";
+    jwt.verify(token, secret, (err: any, decoded: any) => {
       if (err) {
-        return res.status(401).json({ message: "Token tidak valid" });
+        req.flash("alertMessage", "Token tidak valid");
+        req.flash("alertStatus", "danger");
+        return res.redirect("/admin/signin");
       }
+      req.decoded = decoded;
       next();
     });
   } catch (error) {
     if (error instanceof jwt.TokenExpiredError) {
-      return res
-        .status(401)
-        .json({ message: "Token kedaluwarsa, akses ditolak" });
+      req.flash("alertMessage", "Your session expired, please login again !");
+      req.flash("alertStatus", "danger");
+      return res.redirect("/admin/signin");
     }
-    return res
-      .status(401)
-      .json({ message: "Token tidak valid, akses ditolak" });
+    req.flash("alertMessage", "Token tidak valid, akses ditolak");
+    req.flash("alertStatus", "danger");
+    return res.redirect("/admin/signin");
   }
 };
 
-const isLogin = (req: Request, res: Response, next: NextFunction) => {
-  if (req.session.user == null || req.session.user == undefined) {
-    console.log("Active", req.session);
-    req.flash("alertMessage", "Your session expired, please login again !");
-    req.flash("alertStatus", "danger");
-    res.redirect("/admin/signin");
-  } else {
-    next();
-  }
-};
 
 export default {
   isAuthenticated,
-  isLogin,
 };
